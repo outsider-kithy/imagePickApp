c1a064fde8350863e3366f5a0b35bea43425b77a
outsider-kithy <kishida@outsider-design-tech.net>
2024-08-08 15:02:15 +0900

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, Image, View, StyleSheet } from 'react-native';
+import * as ImagePicker from "expo-image-picker";
 
-export default function App() {
-  return (
+export default function App(){
+  const [image, setImage] = useState(null);
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    //console.log(result);
+
+    if(!result.canceled){
+      setImage(result.assets[0].uri);
+    }
+  }
+
+  return(
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button title='カメラロールから画像を選択' onPress={pickImage} />
+      {image && <Image source={{ uri: image }} style={styles.image} /> }
     </View>
-  );
+  )
+
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    aliginItems: 'center',
     justifyContent: 'center',
   },
-});
+  image:{
+    width: 200,
+    height: 200,
+    margin: 'auto',
+  },
+})
---
+++ README.md
@@ -0,0 +1,2 @@
+# React Native(Expo)でカメラロールから画像を選択
+`expo-image-picker`でスマホのカメラロールから画像を選択
